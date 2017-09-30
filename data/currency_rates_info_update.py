e4cb92b4f9a1e26d5d7af2ff98b33e9d54866ada
Anton Morozov <frostman13@mail.ru>
2017-09-30 17:37:15 +0300

    minor fix

--- data/currency_rates_info_update.py
+++ data/currency_rates_info_update.py
@@ -1,6 +1,7 @@
 import urllib.request
 import xml.etree.ElementTree as etree
-from datetime import datetime, date, time, timedelta
+import pytz
+from datetime import datetime, date, timedelta
 
 CURRENCY_URL = 'https://www.cbr.ru/scripts/XML_daily.asp'
 FOLDER_PATH = '/home/ubuntu/diplom_project/data/' # если запускается локально, обнулить
@@ -13,28 +14,34 @@ def get_xml_file_name(url):
     xml_file_name = url_split.replace(ext_split, "xml")
     return str.lower(xml_file_name)
 
-xml_file_name = get_xml_file_name(CURRENCY_URL)
-local_time = datetime.now()+TIME_DELTA
-
-def get_day():
-    with open(FOLDER_PATH+xml_file_name, 'rb') as f:
+def get_day(filename):
+    with open(filename, 'rb') as f:
         tree = etree.parse(f)
         root = tree.getroot()
         return root.attrib['Date']
 
-def get_currency_info():
+def get_currency_info(filename, logname):
     web_file = urllib.request.urlopen(CURRENCY_URL, timeout = 10)
     web_data = web_file.read()
-    with open(FOLDER_PATH+xml_file_name, "wb") as local_file:
+    with open(filename, "wb") as local_file:
         local_file.write(web_data)
-    with open(FOLDER_PATH+'currency_data_log.txt', "a") as currency_data_log:
-        currency_data_log.write('{};информация обновлена (файл теперь от {})\n'.format(local_time,get_day()))
+    with open(logname, "a") as currency_data_log:
+        currency_data_log.write('{};информация обновлена (курсы теперь от {})\n'.format(local_time, get_day(filename)))
 
-try:    
-    if get_day() > datetime.date(datetime.now()+TIME_DELTA).strftime('%d.%m.%Y'):
-        with open(FOLDER_PATH+'currency_data_log.txt', "a") as currency_data_log:
-            currency_data_log.write('{};информация актуальна (файл теперь от {})\n'.format(local_time,get_day()))
-    else:
-        get_currency_info()
-except FileNotFoundError:
-        get_currency_info()
+if __name__ == '__main__':
+    xml_file_name = get_xml_file_name(CURRENCY_URL)
+    file_fullname = FOLDER_PATH + xml_file_name
+    log_fullname = FOLDER_PATH + 'currency_data_log.txt'
+    local_time = datetime.now(pytz.timezone('Europe/Moscow')).replace(tzinfo=None)
+
+    try:
+        file_date = datetime.strptime(get_day(file_fullname),'%d.%m.%Y')
+        if file_date > local_time:
+            with open(log_fullname, "a") as currency_data_log:
+                currency_data_log.write('{};информация актуальна (курсы от {})\n' \
+                    .format(local_time, get_day(file_fullname)))
+        else:
+            get_currency_info(file_fullname, log_fullname)
+    except FileNotFoundError:
+            get_currency_info(file_fullname, log_fullname)
+
